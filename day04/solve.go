2c05c66b5b82885918c9bb1fc36825676c3b2dad
Misode <misoloo64@gmail.com>
2024-12-04 06:28:04 +0100

    Day 04

---
+++ day04/solve.go
@@ -0,0 +1,67 @@
+package day04
+
+import (
+	"fmt"
+
+	"misode.dev/aoc-2024/utils"
+)
+
+func Solve() (int, int) {
+	fmt.Println("=== Day 04 ===")
+
+	lines := utils.ReadInput("in.txt")
+
+	G := make([][]rune, 0, len(lines))
+	for _, line := range lines {
+		G = append(G, []rune(line))
+	}
+
+	part1 := 0
+	for r := 0; r < len(G); r++ {
+		for c := 0; c < len(G[0]); c++ {
+			// Horizontal
+			if c+3 < len(G[0]) && G[r][c] == 'X' && G[r][c+1] == 'M' && G[r][c+2] == 'A' && G[r][c+3] == 'S' {
+				part1 += 1
+			}
+			if c+3 < len(G[0]) && G[r][c+3] == 'X' && G[r][c+2] == 'M' && G[r][c+1] == 'A' && G[r][c+0] == 'S' {
+				part1 += 1
+			}
+			// Vertical
+			if r+3 < len(G) && G[r][c] == 'X' && G[r+1][c] == 'M' && G[r+2][c] == 'A' && G[r+3][c] == 'S' {
+				part1 += 1
+			}
+			if r+3 < len(G) && G[r+3][c] == 'X' && G[r+2][c] == 'M' && G[r+1][c] == 'A' && G[r][c] == 'S' {
+				part1 += 1
+			}
+			// Primary diagonal
+			if r+3 < len(G) && c+3 < len(G[0]) && G[r][c] == 'X' && G[r+1][c+1] == 'M' && G[r+2][c+2] == 'A' && G[r+3][c+3] == 'S' {
+				part1 += 1
+			}
+			if r+3 < len(G) && c+3 < len(G[0]) && G[r+3][c+3] == 'X' && G[r+2][c+2] == 'M' && G[r+1][c+1] == 'A' && G[r][c] == 'S' {
+				part1 += 1
+			}
+			// Secondary diagonal
+			if r+3 < len(G) && c+3 < len(G[0]) && G[r][c+3] == 'X' && G[r+1][c+2] == 'M' && G[r+2][c+1] == 'A' && G[r+3][c] == 'S' {
+				part1 += 1
+			}
+			if r+3 < len(G) && c+3 < len(G[0]) && G[r+3][c] == 'X' && G[r+2][c+1] == 'M' && G[r+1][c+2] == 'A' && G[r][c+3] == 'S' {
+				part1 += 1
+			}
+		}
+	}
+	fmt.Println(part1)
+
+	part2 := 0
+	for r := 0; r+2 < len(G); r++ {
+		for c := 0; c+2 < len(G[0]); c++ {
+			primary := (G[r][c] == 'M' && G[r+2][c+2] == 'S') || (G[r+2][c+2] == 'M' && G[r][c] == 'S')
+			secondary := (G[r][c+2] == 'M' && G[r+2][c] == 'S') || (G[r+2][c] == 'M' && G[r][c+2] == 'S')
+			if G[r+1][c+1] == 'A' && primary && secondary {
+				part2 += 1
+			}
+		}
+	}
+	fmt.Println(part2)
+
+	return part1, part2
+}
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"misode.dev/aoc-2024/day01"
 	"misode.dev/aoc-2024/day02"
 	"misode.dev/aoc-2024/day03"
+	"misode.dev/aoc-2024/day04"
 )
 
 func main() {
 	day01.Solve()
 	day02.Solve()
 	day03.Solve()
+	day04.Solve()
 }
